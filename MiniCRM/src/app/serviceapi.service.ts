df25be3338847f4274a0d9d9f2bb947ec5361216
Heiner Acosta Silgado <hacm1997@gmail.com>
2022-07-18 16:54:55 -0500

    Config service api for new bakend Java

--- MiniCRM/src/app/serviceapi.service.ts
+++ MiniCRM/src/app/serviceapi.service.ts
@@ -8,14 +8,14 @@ import { Observable } from 'rxjs';
 export class ServiceapiService {
 
   //Ruta de la API creada en django
-  readonly EndPoint = "http://127.0.0.1:8000/";
+  readonly EndPoint = "http://localhost:8080/";
 
   constructor(private http:HttpClient) {}
     
   
     //Método para obtener los tipos de usuarios
     getType():Observable<any[]>{
-      return this.http.get<any[]>(this.EndPoint + 'restapi/show/');
+      return this.http.get<any[]>(this.EndPoint + 'ApiRest/User/show/');
     }
 
     //Método para agregar un tipo de usuario/cliente
@@ -25,39 +25,39 @@ export class ServiceapiService {
 
     //Método para actualizar un tipo de usuario/cliente
     updateType(val:any){
-      return this.http.put(this.EndPoint + 'restapi/show/', val);
+      return this.http.put(this.EndPoint + 'ApiRest/User/update/', val);
     }
 
     //Mtodo para eliminar un tipo de usuario/cliente
     deleteType(val:any){
-      return this.http.delete(this.EndPoint + 'restapi/show/' + val);
+      return this.http.delete(this.EndPoint + 'ApiRest/User/delete/' + val);
     }
 
     // Métodos de operaciones con los usuarios/clientes //
 
      //Metodo para obtener de mi API la lista de tipo de persona
      getListUser():Observable<any[]>{
-      return this.http.get<any[]>(this.EndPoint + 'restapi/users/show/');
+      return this.http.get<any[]>(this.EndPoint + 'ApiRest/User/show/');
     }
 
     //Metodo para agregar a mi API un tipo de persona
     addUser(val:any){
-      return this.http.post(this.EndPoint + 'restapi/users/add/', val);
+      return this.http.post(this.EndPoint + 'ApiRest/User/add/', val);
     }
 
     //Metodo para actualizar de mi API un tipo de persona
     detailUser(val:any, id:number){
-      return this.http.get<any>(this.EndPoint + 'restapi/users/show/' + id);
+      return this.http.get<any>(this.EndPoint + 'ApiRest/User/show/' + id);
     }
 
     //Metodo para actualizar de mi API un tipo de persona
     updateUser(val:any, id:number){
-      return this.http.put<any>(this.EndPoint + 'restapi/users/show/' + id, val);
+      return this.http.put<any>(this.EndPoint + 'ApiRest/User/update/' + id, val);
     }
 
     //Metodo para eliminar de mi API un tipo de persona
     deleteUsers(id:number){
-      return this.http.delete<any>(this.EndPoint + 'restapi/users/show/' + id);
+      return this.http.delete<any>(this.EndPoint + 'ApiRest/User/delete/' + id);
     }
 
   }
